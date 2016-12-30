b851935b7fb06b7510ca921257c86506525e9ddd
Thomas Sileo <thomas.sileo@gmail.com>
2016-12-30 21:51:11 +0100

    Add some doc

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # defender
 
+[![Godoc Reference](https://godoc.org/github.com/tsileo/defender?status.png)](https://godoc.org/github.com/tsileo/defender)
+
+B
 Low-level package to help prevent brute force attacks, built on top of [golang.org/x/time/rate](https://golang.org/x/time/rate).
+
+```
+package main
+
+import (
+	"time"
+
+	"github.com/tsileo/defender"
+)
+
+func main() {
+	// Ban client for 1 hour if moe than 50 events per seconds are performed
+	d := defender.New(50, 1 * time.Second, 1 * time.Hour)
+	if d.Banned(r.RemoteAddr) {
+		// Do sth
+        }
+}
+```
--- defender.go
+++ defender.go
@@ -9,55 +9,102 @@ import (
 
 const Factor = 50
 
-type client struct {
+type Client struct {
 	limiter *rate.Limiter
 	expire  time.Time
 	banned  bool
+	key     interface{}
 }
 
+func (c *Client) Key() interface{}  { return c.key }
+func (c *Client) Banned() bool      { return c.banned }
+func (c *Client) Expire() time.Time { return c.expire }
+
+// Defender keep tracks if the `Client`s and maintains the banlist
 type Defender struct {
-	tokenBuckets map[string]*client
+	tokenBuckets map[interface{}]*Client
 
 	Duration    time.Duration
 	BanDuration time.Duration
 	Max         int
 
+	BanHook func(interface{}) error
+
 	sync.Mutex
 }
 
+// New initializes a Defender instance that will limit `max` event maximum per `duration` before banning the client for `banDuration`
 func New(max int, duration, banDuration time.Duration) *Defender {
 	return &Defender{
-		tokenBuckets: map[string]*client{},
+		tokenBuckets: map[interface{}]*Client{},
 		Duration:     duration,
 		BanDuration:  banDuration,
 		Max:          max,
 	}
 }
 
-func (d *Defender) Banned(key string) bool {
+// BanList returns the list of banned clients
+func (d *Defender) BanList() []*Client {
+	l := []*Client{}
+	for _, client := range d.tokenBuckets {
+		if client.banned {
+			l = append(l, client)
+		}
+	}
+	return l
+}
+
+// Banned returns true if the client is banned
+func (d *Defender) Banned(key interface{}) (bool, error) {
 	d.Lock()
 	defer d.Unlock()
 	now := time.Now()
+
+	// Try to retrieve the
 	if _, found := d.tokenBuckets[key]; !found {
-		d.tokenBuckets[key] = &client{
+		d.tokenBuckets[key] = &Client{
+			key:     key,
 			limiter: rate.NewLimiter(rate.Every(d.Duration), d.Max),
 			expire:  now.Add(d.Duration * Factor),
 		}
 	}
 
 	client := d.tokenBuckets[key]
-	if client.banned {
-		return true
+
+	// Check if the client is not banned anymore and the cleanup hasn't been run yet
+	if client.banned && now.After(client.expire) {
+		client.banned = false
 	}
+
+	// Check if client is banned
+	if client.banned {
+		return true, nil
+	}
+
+	// Update the client expiration
 	client.expire = now.Add(d.Duration * Factor)
-	banned := !client.limiter.AllowN(now, 1)
+
+	// Check the rate limiter
+	banned := !client.limiter.AllowN(time.Now(), 1)
+
 	if banned {
+		// Set the client as banned
 		client.banned = true
+
+		// Check if a hook is registered
+		if d.BanHook != nil {
+			if err := d.BanHook(key); err != nil {
+				return false, err
+			}
+		}
+		// Set the ban duration
 		client.expire = now.Add(d.BanDuration)
 	}
-	return banned
+
+	return banned, nil
 }
 
+// Cleanup should be used if you want to manage the cleanup yourself, looks for CleanupTask for an automatic way
 func (d *Defender) Cleanup() {
 	d.Lock()
 	defer d.Unlock()
@@ -69,6 +116,7 @@ func (d *Defender) Cleanup() {
 	}
 }
 
+// CleanupTask should be run in a goroutime
 func (d *Defender) CleanupTask(quit <-chan struct{}) {
 	c := time.Tick(d.Duration * Factor)
 	for {
